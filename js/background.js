571b9f1edbcc393d8e569b2298a99ea01d668df6
Meongjin Ki <meongjinki91@gmail.com>
2023-11-11 21:12:07 +0900

    Add random background

---
+++ js/background.js
@@ -0,0 +1,10 @@
+imgs = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg"];
+
+function changeBackground() {
+  var i = Math.floor(Math.random() * imgs.length);
+  document.body.style.backgroundImage = "url(" + imgs[i] + ")";
+}
+
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+changeBackground();
