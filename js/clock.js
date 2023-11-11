853ffb9f921d34d5295c62269f5e652b36145d47
Meongjin Ki <meongjinki91@gmail.com>
2023-11-11 22:11:09 +0900

    Modify clock

--- js/clock.js
+++ js/clock.js
@@ -10,10 +10,9 @@ function getTime() {
 
 function updateClock() {
   const { hours, minutes, seconds } = getTime();
-  const timeString = `
-    ${hours.toString().padStart(2, "0")}:
-    ${minutes.toString().padStart(2, "0")}:
-    ${seconds.toString().padStart(2, "0")}`;
+  const timeString = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   clock.innerText = timeString;
 }
 
